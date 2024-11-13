1f7a24895fcb6bed4b27815014996cf3719239a3
Guilherme Barbosa <barbosa.guisilvaa@gmail.com>
2024-11-13 20:28:49 -0300

    criando uma página pra cada ep

--- app.js
+++ app.js
@@ -1,43 +1,3 @@
-// document.querySelectorAll('.dropdown-content a').forEach(link => {
-//     link.addEventListener('click', function(event) {
-//         event.preventDefault();
-
-//         const videoUrl = this.getAttribute('data-video');
-//         const descricaoTexto = this.getAttribute('data-descricao');
-
-//         const videoContainer = document.getElementById('video-container');
-//         const episodioFrame = document.getElementById('episodio-frame');
-
-//         // Remove qualquer imagem de "em breve" existente
-//         const existingImage = document.getElementById('em-breve-image');
-//         if (existingImage) {
-//             existingImage.remove();
-//         }
-
-//         if (videoUrl === "em breve") {
-//             // Esconde o iframe
-//             episodioFrame.style.display = 'none';
-
-//             // Adiciona uma imagem de "em breve"
-//             const emBreveImage = document.createElement('img');
-//             emBreveImage.src = './assets/breve.png'; // Substitua pelo caminho da imagem de "em breve"
-//             emBreveImage.id = 'em-breve-image';
-//             emBreveImage.alt = 'Em breve';
-//             emBreveImage.style.width = '100%';
-
-//             videoContainer.appendChild(emBreveImage);
-//         } else {
-//             // Exibe o iframe e define o novo link de vídeo
-//             episodioFrame.style.display = 'block';
-//             episodioFrame.src = videoUrl;
-//         }
-
-//         // Atualiza o texto da descrição
-//         document.getElementById('descricao-texto').innerText = descricaoTexto;
-//     });
-// });
-
-
 document.querySelectorAll('.dropdown-content a').forEach(link => {
     link.addEventListener('click', function(event) {
         event.preventDefault();
@@ -98,7 +58,22 @@ document.querySelectorAll('.dropdown-content a').forEach(link => {
             // Atualiza a descrição
             document.getElementById('descricao-texto').innerText = descricaoTexto;
             document.getElementById('estreia-texto').innerText = estreiaTexto;
+            
+        }
+
+        document.getElementById('trailer').style.display = 'none';
+        document.getElementById('sinopse_serie').style.display = 'none';
+        document.getElementById('descricao_container').style.marginTop = '20px'
+
+        const voltarButton = document.querySelector('.voltar');
+        if (voltarButton) {
+            voltarButton.classList.remove('hide');
         }
     });
+});
+
+document.querySelector('.voltar').addEventListener('click', function() {
+    // Recarrega a página ao clicar no botão "Voltar"
+    window.location.reload();
 });
 
